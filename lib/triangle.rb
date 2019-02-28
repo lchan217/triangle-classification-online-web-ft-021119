76aeb5c99d7da6d24ce30374f8846edf442dea86
Lizzie Chan <lchan217@gmail.com>
2019-02-28 17:46:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
     @c = c 
   end 
   def kind
-    if (@a < 0 || @b < 0 || @c < 0) || (@a + @b < @c || @b + @c < @a || @a + @c < @b)
+    if (@a < 0 || @b < 0 || @c < 0) || (@a + @b <= @c || @b + @c <= @a || @a + @c <= @b)
      begin
         raise TriangleError
       # rescue TriangleError => error
