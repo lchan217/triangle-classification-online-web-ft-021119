e596fb12c53fbcb1d7989def5f40e2977fac5831
Lizzie Chan <lchan217@gmail.com>
2019-02-28 16:32:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c 
+  def initialize(a,b,c)
+    @a = a 
+    @b = b 
+    @c = c 
+  end 
+  def kind 
+    #if triangle invalid -> Custom Error 
+      #sum of the lengths of any two sides of a triangle always exceeds the length of the third side
+      #each side > 0 
+     
+    #elsif equil - 3 equal sides 
+      
+    #elsif isos - 2 equal sides 
+     
+    #else scalene - no equal 
+      
+  end 
+  class TriangleError < StandardError 
+    def message 
+      "invalid triangle"
+    end 
+  end 
 end
